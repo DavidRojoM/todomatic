e6d65258b0724d7e1d69f0ae08178e5433cf65f6
David Rojo <davidrojo1997@gmail.com>
2021-11-22 19:33:40 +0100

    feat(todo-list): add localstorage rendering feature

--- frontend/src/app/components/todo-list/todo-list.component.ts
+++ frontend/src/app/components/todo-list/todo-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { TodosRepositoryService } from '../../services/repository/todos-repository.service';
+import { retrievedTodoList } from '../../state/actions/todos.actions';
+import { Observable } from 'rxjs';
+import { Todo } from '../../models/todo.interface';
+import { selectTodos } from '../../state/selectors/todos.selector';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  todoList$: Observable<readonly Todo[]> = new Observable();
+  constructor(
+    private readonly todosRepository: TodosRepositoryService,
+    private store: Store
+  ) {
+    this.todoList$ = this.store.select(selectTodos);
   }
 
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  loadTodos() {
+    this.todosRepository.fetchTodos().subscribe((todos) => {
+      this.store.dispatch(retrievedTodoList({ todos }));
+    });
+  }
 }
