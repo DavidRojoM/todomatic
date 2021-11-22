5df6ebcb5de3547707adfa90c470ff105833bb94
David Rojo <davidrojo1997@gmail.com>
2021-11-22 20:09:31 +0100

    feat(app.module): add redux debugger

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,10 +6,19 @@ import { StoreModule } from '@ngrx/store';
 import { ROOT_REDUCERS } from './state/app.state';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent, TodoItemComponent],
-  imports: [BrowserModule, StoreModule.forRoot(ROOT_REDUCERS)],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot(ROOT_REDUCERS),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: true, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
