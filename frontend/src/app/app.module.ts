7e4387c493ad98c4cefa2b755ec494582d73054f
David Rojo <davidrojo1997@gmail.com>
2021-11-22 18:33:17 +0100

    feat(components): add todo-list and todo-item components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { ROOT_REDUCERS } from './state/app.state';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoItemComponent } from './components/todo-item/todo-item.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, TodoListComponent, TodoItemComponent],
   imports: [BrowserModule, StoreModule.forRoot(ROOT_REDUCERS)],
   providers: [],
   bootstrap: [AppComponent],
